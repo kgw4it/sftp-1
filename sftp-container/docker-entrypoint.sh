8ecbb489c9fcf12c27eeecb36e1bce9c517e9b64
kgw4it <30734393+kgw4it@users.noreply.github.com>
2020-03-22 19:03:46 +0100

    Update docker-entrypoint.sh

--- sftp-container/docker-entrypoint.sh
+++ sftp-container/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 sed s/1000/`id -u`/g /etc/passwd > /tmp/passwd
 cat /tmp/passwd > /etc/passwd
 echo -e "thispasswordneedstobechanged\n$SFTP_PASSWORD\n$SFTP_PASSWORD" | passwd
-mkdir /home/$SFTP_USER/.ssh
+
 echo -e $SFTP_PUBLIC_KEY > /home/$SFTP_USER/.ssh/authorized_keys
 chmod 600 /home/$SFTP_USER/.ssh
 chmod 700 /home/$SFTP_USER/.ssh/authorized_keys
