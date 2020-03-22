63683ed37d9b9a70a135f62f6ff6437f3e8b432f
kgw4it <30734393+kgw4it@users.noreply.github.com>
2020-03-22 20:34:32 +0100

    Update docker-entrypoint.sh

--- sftp-container/docker-entrypoint.sh
+++ sftp-container/docker-entrypoint.sh
@@ -13,14 +13,14 @@ echo -e "thispasswordneedstobechanged\n$SFTP_PASSWORD\n$SFTP_PASSWORD" | passwd
 
 # Set Host Keys
 if [ -n "$SSH_HOST_ED25519_KEY" ]; then
-    echo -e $SSH_HOST_ED25519_KEY > /tmp/ssh_host_ed25519_key
+    echo -e "$SSH_HOST_ED25519_KEY" > /tmp/ssh_host_ed25519_key
     chmod 600 /tmp/ssh_host_ed25519_key
 else
 echo "SSH_HOST_ED25519_KEY Environment variable not defined!"
 fi
 
 if [ -n "$SSH_HOST_RSA_KEY" ]; then
-    echo -e $SSH_HOST_RSA_KEY > /tmp/ssh_host_rsa_key
+    echo -e "$SSH_HOST_RSA_KEY" > /tmp/ssh_host_rsa_key
     chmod 600 /tmp/ssh_host_rsa_key
 else
 echo "SSH_HOST_RSA_KEY Environment variable not defined!"
